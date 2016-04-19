01fd5a304876450b776f7ba6bd29c56bd7026401
eorigbo <eorigbo@gmail.com>
2016-04-19 20:49:35 +0000

    users model and schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411234009) do
+ActiveRecord::Schema.define(version: 20160419204003) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

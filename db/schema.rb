199ceccc0f9f86d93de884ae3f9cc3a1ca541254
eorigbo <eorigbo@gmail.com>
2016-04-20 22:14:54 +0000

    articles users association

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419204003) do
+ActiveRecord::Schema.define(version: 20160420194255) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
